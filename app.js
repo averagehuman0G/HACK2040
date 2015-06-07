e5f975b32541ca5fbeb947e6cd4365054e872b4d
Joe Jean <joeclef@hotmail.com>
2015-06-06 22:54:15 -0400

    beta version

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ nunjucks.configure("views", {
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
